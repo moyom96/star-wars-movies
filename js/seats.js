4b7a9f41e7b3e6ba87177ea8c595fdaf76531c8b
Bernardo <os.bernardo@outlook.com>
2021-09-21 01:20:06 -0500

    Change seat

--- js/seats.js
+++ js/seats.js
@@ -1,30 +1,27 @@
 // texture
 class Seat {
-    constructor(x, y, z, door){
+  constructor(x, y, z, door) {
+    var loader = new THREE.STLLoader();
+    loader.load('assets/chair.stl', function (geometry) {
+      var material = new THREE.MeshStandardMaterial({ color: 'red' });
 
-        var loader = new THREE.STLLoader();
-        loader.load( 'assets/chairstl.stl', function ( geometry ) {
+      var seat = new THREE.Mesh(geometry, material);
 
-            var material = new THREE.MeshStandardMaterial( { color: 'red' } );
+      seat.position.x = x;
+      seat.position.y = y;
+      seat.position.z = z;
+      seat.scale.x = 0.017;
+      seat.scale.y = 0.017;
+      seat.scale.z = 0.017;
 
-            var seat = new THREE.Mesh( geometry, material );
+      if (door == 'F') {
+        seat.rotation.y = Math.PI / 2;
+      } else {
+        seat.rotation.y = -Math.PI / 2;
+      }
 
-            seat.position.x = x;
-            seat.position.y = y;
-            seat.position.z = z;
-            seat.scale.x = .017;
-            seat.scale.y = .017;
-            seat.scale.z = .017;
-
-            if (door == "F"){
-                seat.rotation.y = Math.PI/2;
-            } else {
-              seat.rotation.y = -Math.PI/2;
-            }
-
-            scene.add( seat );
-            objects.push(seat);
-
-        } );
-    }
+      scene.add(seat);
+      objects.push(seat);
+    });
+  }
 }
